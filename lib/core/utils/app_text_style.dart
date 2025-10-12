738b8d34f960a47dfff09af2085eeeef075ea2e2
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-10-12 10:30:49 +0300

    creating the utils and helper folders in the core folder and also create the text style for the whole app and also a helper file for responsive text

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/widgets.dart';
+import 'package:car_zone/core/helpers/text_responsive_helper.dart';
+
+class AppTextStyle {
+  final BuildContext context;
+
+  AppTextStyle(this.context);
+
+  TextStyle get text10Regular => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 10, context: context),
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get text12Regular => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 12, context: context),
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get text14Regular => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 14, context: context),
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get text14Bold => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 14, context: context),
+        fontWeight: FontWeight.bold,
+      );
+
+  TextStyle get text16Regular => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 16, context: context),
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get text16Bold => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 16, context: context),
+        fontWeight: FontWeight.bold,
+      );
+
+  TextStyle get text18SemiBold => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 18, context: context),
+        fontWeight: FontWeight.w600,
+      );
+
+  TextStyle get text20Bold => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 20, context: context),
+        fontWeight: FontWeight.bold,
+      );
+
+  TextStyle get text24Bold => TextStyle(
+        fontSize: TextResponsiveHelper.responsiveText(
+            fontSize: 24, context: context),
+        fontWeight: FontWeight.bold,
+      );
+}
+
+extension AppTextStyleExtension on BuildContext {
+  AppTextStyle get textStyle => AppTextStyle(this);
+}
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  static const String imagesLogo = "assets/images/logo.png";
+}
+
