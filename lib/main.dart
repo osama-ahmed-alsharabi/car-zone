7ba42434abd72385e0c96e403fb642673aaaf867
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-10-12 09:24:35 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarZone());
+}
+
+class CarZone extends StatelessWidget {
+  const CarZone({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
