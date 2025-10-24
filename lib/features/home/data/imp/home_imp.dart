46c2f3721daa44ef719d63361b8074bfeb9e0414
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-10-25 00:42:28 +0300

    done with the method that get the products

--- lib/features/home/data/imp/home_imp.dart
+++ lib/features/home/data/imp/home_imp.dart
@@ -1,6 +1,7 @@
 import 'package:car_zone/core/helpers/api_helper.dart';
 import 'package:car_zone/core/helpers/backend_result.dart';
 import 'package:car_zone/features/home/data/model/brand_model.dart';
+import 'package:car_zone/features/home/data/model/car_model.dart';
 import 'package:car_zone/features/home/data/repo/home_repo.dart';
 
 class HomeImp extends HomeRepo {
@@ -19,6 +20,23 @@ class HomeImp extends HomeRepo {
           .map((e) => BrandModel.fromJson(e))
           .toList();
       return Success(brands);
+    } else {
+      return Failure("error");
+    }
+  }
+
+  @override
+  Future<BackendResult<List<CarModel>, String>> getCars() async{
+    BackendResult result = await api.get(
+      endPoint: "products",
+      contentType: "",
+      accept: "",
+    );
+    if (result is Success) {
+      List<CarModel> cars = (result.value["data"] as List<dynamic>)
+          .map((e) => CarModel.fromJson(e))
+          .toList();
+      return Success(cars);
     } else {
       return Failure("error");
     }
--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:car_zone/core/helpers/backend_result.dart';
 import 'package:car_zone/features/home/data/model/brand_model.dart';
+import 'package:car_zone/features/home/data/model/car_model.dart';
 
 abstract class HomeRepo {
   Future<BackendResult<List<BrandModel>, String>> getBarnd();
+  Future<BackendResult<List<CarModel>, String>> getCars();
 }
