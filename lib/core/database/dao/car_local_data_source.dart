58d9d2895de760c2029730b5747504cddd0eea9b
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-10-28 21:22:10 +0300

    refactor the file of car lacol data source and make the appdatabase to depend on one object

--- lib/core/database/dao/car_local_data_source.dart
+++ lib/core/database/dao/car_local_data_source.dart
@@ -4,38 +4,35 @@ import 'package:car_zone/features/home/data/model/images_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class CarLocalDataSource {
+  final dbProvider = AppDatabase();
   Future<void> cacheCars(List<CarModel> cars) async {
-    final db = await AppDatabase().database;
+    final db = await dbProvider.database;
 
     await db.delete('cars');
 
     for (var car in cars) {
-      await db.insert(
-        'cars',
-        {
-          'id': car.id,
-          'title': car.title,
-          'description': car.description,
-          'model': car.model.toString(),
-          'price': car.price,
-          'condition': car.condition,
-          'engine_cylinders': car.engineCylinders,
-          'fuel_type': car.fuelType,
-          'latitude': car.latitude.toString(),
-          'longitude': car.longitude.toString(),
-          'brand_id': car.brandId,
-          'province_id': car.provinceId,
-          'user_id': car.userId,
-          'main_image': car.images.mainImage,
-          'sub_image': car.images.subImage,
-        },
-        conflictAlgorithm: ConflictAlgorithm.replace,
-      );
+      await db.insert('cars', {
+        'id': car.id,
+        'title': car.title,
+        'description': car.description,
+        'model': car.model.toString(),
+        'price': car.price,
+        'condition': car.condition,
+        'engine_cylinders': car.engineCylinders,
+        'fuel_type': car.fuelType,
+        'latitude': car.latitude.toString(),
+        'longitude': car.longitude.toString(),
+        'brand_id': car.brandId,
+        'province_id': car.provinceId,
+        'user_id': car.userId,
+        'main_image': car.images.mainImage,
+        'sub_image': car.images.subImage,
+      }, conflictAlgorithm: ConflictAlgorithm.replace);
     }
   }
 
   Future<List<CarModel>> getCachedCars() async {
-    final db = await AppDatabase().database;
+    final db = await dbProvider.database;
     final data = await db.query('cars');
 
     return data.map((e) {
@@ -54,7 +51,7 @@ class CarLocalDataSource {
         provinceId: e['province_id'] as int,
         userId: e['user_id'] as int,
         images: ImagesModel(
-          mainImage: e['main_image']  as String,
+          mainImage: e['main_image'] as String,
           subImage: e['sub_image'] as String,
         ),
       );
