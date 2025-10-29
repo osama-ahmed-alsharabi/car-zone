10f3919cc3f52132ab11f895c825ca735fc45700
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-10-29 20:15:03 +0300

    starting the marketing text to make the user sell the car and add the car name

--- lib/features/profile/presentation/view/widgets/create_product_body.dart
+++ lib/features/profile/presentation/view/widgets/create_product_body.dart
@@ -1,13 +1,32 @@
-
+import 'package:car_zone/core/utils/app_colors.dart';
+import 'package:car_zone/core/utils/app_text_style.dart';
+import 'package:car_zone/core/widgets/custom_text_from_field_widget.dart';
 import 'package:flutter/material.dart';
 
 class CreateProductBody extends StatelessWidget {
-  const CreateProductBody({
-    super.key,
-  });
+  const CreateProductBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 15),
+          Text(
+            "ابدأ البيع بخطوة بسيطة… والباقي علينا!",
+            style: context.textStyle.text18SemiBold.copyWith(
+              color: AppColors.primaryColor,
+            ),
+          ),
+          SizedBox(height: 30),
+          CustomTextFormFieldWidget(
+            labelText: "اسم السيارة",
+            icon: Icons.title,
+          ),
+        ],
+      ),
+    );
   }
 }
